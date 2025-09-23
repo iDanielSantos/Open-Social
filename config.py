65d1418c0401ad97c480b04ee7c4c23f3f6e88d9
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-23 11:54:30 -0300

    feat: add configuration for environment variables and MariaDB

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    pass
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # MariaDB
+    MARIADB_HOST = os.getenv("MARIADB_HOST")
+    MARIADB_PORT = int(os.getenv("MARIADB_PORT"))
+    MARIADB_USER = os.getenv("MARIADB_USER")
+    MARIADB_PASSWORD = os.getenv("MARIADB_PASSWORD")
+    MARIADB_DB = os.getenv("MARIADB_DB")
