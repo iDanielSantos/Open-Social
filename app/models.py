e91bdbbeb629e37e1fb4cac4ff833db310465bbd
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-23 12:10:20 -0300

    feat: implement MariaDB connection and Bcrypt initialization

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from flask_bcrypt import Bcrypt
+import mariadb
+import config as Config
+
+bcrypt = Bcrypt()
+
+def get_db_connection():
+    return mariadb.connect(
+        host=Config.MARIADB_HOST,
+        port=Config.MARIADB_PORT,
+        user=Config.MARIADB_USER,
+        password=Config.MARIADB_PASSWORD,
+        database=Config.MARIADB_DB
+    )
