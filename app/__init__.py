005cc6355de15e4954da4f40273edfd60a41b4a7
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-23 12:13:46 -0300

    feat: initialize Bcrypt in Flask application context

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from .models import bcrypt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     with app.app_context():
+        bcrypt.init_app(app)
+
         return app
