ee89b8977ebe3650a9f5b0431cfbed92369e34f4
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-23 16:55:21 -0300

    feat: register auth_bp blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ def create_app():
     app.config.from_object('config.Config')
 
     with app.app_context():
-        from .routes import main_bp
+        from .routes import main_bp, auth_bp
         app.register_blueprint(main_bp)
+        app.register_blueprint(auth_bp)
 
         bcrypt.init_app(app)
 
