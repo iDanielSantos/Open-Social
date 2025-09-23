cd314eb90d68d252244b8e5b6c53bf5e2869e13c
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-23 15:59:50 -0300

    refactor: use context manager for connection and cursor

--- app/models.py
+++ app/models.py
@@ -14,22 +14,16 @@ def get_db_connection():
     )
 
 def get_user_by_username(username):
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT username, password_hash FROM users WHERE username = ?", (username,))
-    user = cur.fetchone()
-    cur.close()
-    conn.close()
-    return user
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT username, password_hash FROM users WHERE username = ?", (username,))
+            return cur.fetchone()
 
 def add_user_to_db(username, display_name, password):
     password_hash = bcrypt.generate_password_hash(password).decode('utf-8') 
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO users (username, display_name, password_hash) VALUES (?, ?, ?)", (username, display_name, password_hash))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO users (username, display_name, password_hash) VALUES (?, ?, ?)", (username, display_name, password_hash))
 
 def check_password(password_hash, password):
     return bcrypt.check_password_hash(password_hash, password)
