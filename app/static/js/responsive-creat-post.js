2ea7b90702785952cd7708a608ffcabad898d71b
Gustavo-Ftz <gustavoftz12@outlook.com>
2025-09-29 17:08:15 -0300

    style: build userProfile and adjust responsive

--- app/static/js/responsive-creat-post.js
+++
@@ -1,9 +0,0 @@
-// Testing functions
-
-function delMain() {
-    const newElement = window.document.createElement('p');
-    newElement.textContent = 'Olá, Mundo!';
-    window.document.body.appendChild(newElement)
-}
-
-delMain()
