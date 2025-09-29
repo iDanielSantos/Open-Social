a5370d4a5df99ce1386b2ac226a48ba953e7f6f8
Gustavo-Ftz <gustavoftz12@outlook.com>
2025-09-29 17:11:08 -0300

    test: add responsive userProfile

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,18 @@
-window.alert('Bem vindo(a)!')
+const navitationMenu = document.querySelectorAll("nav")[0];
+const userProfile = document.getElementById("userProfile");
+const btMenu = document.getElementById("btMenu");
+
+function hideMenu() {
+  if (navitationMenu.style.display == "none") {
+    navitationMenu.style.display = "inline-block";
+    userProfile.style.backgroundColor = "var(--color2)";
+    userProfile.style.borderRight = "2px solid black";
+    
+  } else {
+    navitationMenu.style.display = "none";
+    userProfile.style.backgroundColor = "white";
+    userProfile.style.border = "none";
+  }
+}
+
+btMenu.addEventListener("click", hideMenu);
